2d1f9579aa9e1c50e8cd83f8c3343ab39971da6d
shican45 <1369344031@qq.com>
2021-05-04 11:04:01 +0800

    修改

--- service/team_charge_service.go
+++ service/team_charge_service.go
@@ -29,18 +29,14 @@ func (service *TeamManagementService) Create(c *gin.Context, team model.Team) se
 	team.ClassID = service.ClassID
 
 	res, err := service.SetTeam(team)
-	if errors.Is(err, gorm.ErrRecordNotFound) {
-		return serializer.ParamErr("创建团队错误，未发现记录", nil)
-	}
-	if errors.Is(err, gorm.ErrInvalidField) {
-		return serializer.ParamErr("创建团队错误，字段的值无效", nil)
-	}
-	if errors.Is(err, gorm.ErrInvalidValue) {
+	if err != nil{
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return serializer.ParamErr("创建团队错误", nil)
+		}
+	} else {
 		return serializer.ParamErr("创建团队错误，值无效", nil)
 	}
-	if errors.Is(err, gorm.ErrInvalidValueOfLength) {
-		return serializer.ParamErr("创建团队错误，值的长度过长", nil)
-	}
+
 	if res == 0 {
 		return serializer.ParamErr("创建团队错误", nil)
 	}
