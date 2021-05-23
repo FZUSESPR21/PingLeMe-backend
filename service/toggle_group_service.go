037c8dacfb814f464e4041a028e2e70ffb6a4879
郑麟轩 <zhenglinxuan@hotmail.com>
2021-05-24 00:35:35 +0800

    完善router。增加开始结对组队功能。修复了一些问题。

---
+++ service/toggle_group_service.go
@@ -0,0 +1,41 @@
+package service
+
+import (
+	"PingLeMe-Backend/cache"
+	"PingLeMe-Backend/serializer"
+	"PingLeMe-Backend/util"
+	"errors"
+	"github.com/go-redis/redis/v8"
+	"go.uber.org/zap"
+	"strconv"
+	"time"
+)
+
+type ToggleGroupService struct {
+}
+
+func (service *ToggleGroupService) ToggleGroup(classID uint, duration time.Duration, str string) serializer.Response {
+	val, err := cache.Get(strconv.Itoa(int(classID)), str)
+	if errors.Is(err, redis.Nil) || val != "true" {
+		ok, err := cache.SetNX(strconv.Itoa(int(classID))+str, "true", duration)
+		if err != nil {
+			util.Log().Error(zap.Error(err).String)
+			return serializer.ServerInnerErr("Redis error:", err)
+		} else if ok == false {
+			util.Log().Error("Redis SetNX get false.")
+			return serializer.ServerInnerErr("Redis SetNX get false.", err)
+		}
+	} else if err == nil {
+		_, err := cache.Del(strconv.Itoa(int(classID)) + str)
+		if err != nil {
+			util.Log().Error(zap.Error(err).String)
+			return serializer.ServerInnerErr("Redis error:", err)
+		}
+	} else {
+		util.Log().Error(zap.Error(err).String)
+		return serializer.ServerInnerErr("Redis error:", err)
+	}
+	return serializer.Response{
+		Code: 0,
+	}
+}
