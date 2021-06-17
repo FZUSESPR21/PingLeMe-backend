d0678e0c54cb71f8e20449ca9b7cbfd13d5c3527
Zheng Linxuan <zhenglinxuan@hotmail.com>
2021-06-17 22:10:50 +0800

    fix file name.

--- api/template.go
+++ api/template.go
@@ -10,7 +10,7 @@ const StudentImportTemplateFilePath = "./template/student_import.xls"
 // StudentImportTemplate 下载文件
 func StudentImportTemplate(c *gin.Context) {
 	c.Writer.WriteHeader(http.StatusOK)
-	c.Header("Content-Disposition", "attachment; filename=student_import.xls")
+	c.Header("Content-Disposition", "attachment; filename=学生导入模版.xls")
 	c.Header("Content-Type", "application/x-xls")
 	c.File(StudentImportTemplateFilePath)
 }
